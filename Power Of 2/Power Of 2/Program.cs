372bc8b8786c0fcc3d60eea571db101f1da63df8
lovelotus <opera.prem@gmail.com>
2022-07-21 20:39:41 +0530

    Added Power of 2 Program

---
+++ Power Of 2/Power Of 2/Program.cs
@@ -0,0 +1,11 @@
+﻿  Console.WriteLine("Enter the values of Power of Two");
+  int n = int.Parse(Console.ReadLine());
+  Console.WriteLine(" table is ");
+
+            for (int i = 0; i < n; i++)
+            {
+                double p = Math.Pow(2, i);
+                Console.WriteLine(p);
+            }
+        
+    
