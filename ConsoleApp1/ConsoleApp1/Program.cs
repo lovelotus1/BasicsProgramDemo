98492ed2ad2fa5730430a6416e8752119306edcf
lovelotus <opera.prem@gmail.com>
2022-07-22 13:40:48 +0530

    Added Fibonacci Program

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci
+{
+    class Program
+
+    {
+        int n = 10;
+        int n1 = 0;
+        int n2 = 1;
+        static void Main(string[] args)
+        {
+            int n = 10;
+            int n1 = 0;
+            int n2 = 1;
+            Console.WriteLine(n1 + " " + n2 + " ");
+            for (int i = 2; i <= n - 1; i++)
+            {
+                int n3 = n1 + n2;
+                n1 = n2;
+                n2 = n3;
+                //Console.WriteLine(i);
+                Console.WriteLine(n3 + " ");
+            }
+        }
+    }
+}
